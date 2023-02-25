a23c5293e4fbb4980e1e5313e136346ba531bb87
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2023-02-25 12:28:10 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  const handleLogoClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setReverse(!reverse);
+    incrementCounter();
+  };
+
+  const incrementCounter = () => setCounter((c) => c + 1);
+  const decrementCounter = () => setCounter((c) => c - 1);
+
+  const reverseClass = reverse ? 'reverse' : '';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" onClick={handleLogoClick} />
+        <p>Clicked {counter} times</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
